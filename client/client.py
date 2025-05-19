d768c2476198d06d6f6b4d906bd283b481eab1af
Ems <emsardellini@gmail.com>
2025-05-19 10:30:51 +0200

    squid nel docker-compose

--- client/client.py
+++ client/client.py
@@ -1,4 +1,13 @@
 import requests
+import time
 
-response = requests.get("http://server:8000")
-print("Server says:", response.text)
+try:
+    print("[Client] Sending request to server...")
+    response = requests.get("http://server:8000")
+    print("Server says:", response.text)
+except Exception as e:
+    print("[Client] Request failed:", e)
+
+# Mantieni il container attivo per ispezione
+while True:
+    time.sleep(100)
