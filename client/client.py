a638640e08f8f1f60701132faea26661e22f4cc8
FedericoPaolucci <s1116751@studenti.univpm.it>
2025-05-23 00:49:19 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -10,8 +10,9 @@ import random
 
 BASE_URL = os.getenv("SERVER_URL", "http://server:8000")
 
+ALL_USER_IDS = list(range(1, 21)) # lista di tutti gli utenti
 OWNER_IDS = [1, 2, 4, 5, 7, 8, 10, 11, 13, 16]  # solo manager/impiegato
-#EXISTING_DRAWING_IDS = list(range(1, 11))  # ipotetici ID esistenti
+EXISTING_DRAWING_IDS = list(range(1, 11))  # ipotetici ID esistenti
 
 def get_random_payload(modify_name_only=False):
     if modify_name_only:
@@ -33,25 +34,25 @@ TEST_SEQUENCE = [
         "action": "POST",
         "drawing_id": None,
         "payload": lambda: get_random_payload(),  # usa funzione
-        "user_id": 1
+        "user_id": lambda: random.choice(OWNER_IDS) # random tra gli utenti manager e impiegati; si può cambiare con ALL_USER_IDS in quanto il server gestisce i permessi.
     },
     {
         "action": "GET_ONE",
-        "drawing_id": 1,
+        "drawing_id": lambda: random.choice(EXISTING_DRAWING_IDS),
         "payload": None,
-        "user_id": 10
+        "user_id": lambda: random.choice(ALL_USER_IDS)
     },
     {
         "action": "PUT",
-        "drawing_id": 1,
+        "drawing_id": lambda: random.choice(EXISTING_DRAWING_IDS),
         "payload": lambda: get_random_payload(modify_name_only=True),
-        "user_id": 10
+        "user_id": lambda: random.choice(OWNER_IDS)
     },
     {
         "action": "DELETE",
-        "drawing_id": 1,
+        "drawing_id": lambda: random.choice(EXISTING_DRAWING_IDS),
         "payload": None,
-        "user_id": 10
+        "user_id": lambda: random.choice(OWNER_IDS)
     },
 ]
 
@@ -59,8 +60,8 @@ def run():
     while True: # cicla infinitamente nella sequenza
         for req in TEST_SEQUENCE:
             action = req["action"]
-            user_id = req["user_id"]
-            drawing_id = req["drawing_id"]
+            user_id = req["user_id"]() if callable(req["user_id"]) else req["user_id"]
+            drawing_id = req["drawing_id"]() if callable(req["drawing_id"]) else req["drawing_id"]
             payload = req["payload"]() if callable(req["payload"]) else req["payload"]
 
             print(f"\n - {action} da user {user_id}")
@@ -73,6 +74,10 @@ def run():
 
                 elif action == "POST":
                     resp = requests.post(f"{BASE_URL}/drawings", json=payload, headers=headers, timeout=5)
+                    result = resp.json()
+                    new_id = result.get("id")
+                    if new_id and new_id not in EXISTING_DRAWING_IDS:
+                        EXISTING_DRAWING_IDS.append(new_id)
                     print(f"[POST] -> status_code: {resp.status_code} | {resp.json()}")
 
                 elif action == "PUT":
@@ -82,6 +87,9 @@ def run():
                 elif action == "DELETE":
                     resp = requests.delete(f"{BASE_URL}/drawings/{drawing_id}", headers=headers, timeout=5)
                     print(f"[DELETE] drawing_id: {drawing_id} -> status_code: {resp.status_code} | {resp.json()}")
+                    if action == "DELETE" and resp.status_code == 200:
+                        if drawing_id in EXISTING_DRAWING_IDS:
+                            EXISTING_DRAWING_IDS.remove(drawing_id)
 
             except Exception as e:
                 print(f"[ERROR] {action} fallita: {e}")
