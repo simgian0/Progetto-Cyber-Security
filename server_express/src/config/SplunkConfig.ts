b8069aa9e499baf052cc6c3154effb2d470de4b4
Elisa Pace <pace.elisa1@gmail.com>
2025-07-21 17:06:20 +0200

    Update SplunkConfig.ts from localhost:8089 to splunk:8089 and it worked

--- server_express/src/config/SplunkConfig.ts
+++ server_express/src/config/SplunkConfig.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 //TO IMPROVE: Usa OAuth2 o Token temporanei se Splunk lo supporta.
 export const SplunkConfig = {
-  host: process.env.SPLUNK_BASE_URL || 'https://localhost:8089',
+  host: process.env.SPLUNK_BASE_URL || 'https://splunk:8089',
   username: process.env.SPLUNK_USERNAME || 'admin',
   password: process.env.SPLUNK_PASSWORD || 'Chang3d!',
 };
